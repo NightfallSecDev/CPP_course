035dd1d9f3fa3b6c309846f6214e5f4c2fcf8e45
NightfallSecDev <m1000tech007@gmail.com>
2024-12-05 15:16:56 +0530

    Create data_type.cpp

---
+++ Part-4/data_type.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // data types in C++
+    int a = 3; // integer
+    double b = 5.4; // double
+    char c = 'A'; // character
+    bool d = true; // boolean
+    string e = "Hello friend"; // string
+
+    cout << "Your integer variable is: " << a << endl;
+    cout << "Your double variable is: " << b << endl;
+    cout << "Your character variable is: " << c << endl;
+    cout << "Your boolean variable is: " << d << endl;
+    cout << "Your string variable is: " << e << endl;
+}
