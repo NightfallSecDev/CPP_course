0d9f5e37a67c7394ace30105f20d8f880c80f6f7
NightfallSecDev <m1000tech007@gmail.com>
2024-12-05 15:07:50 +0530

    Create comment.cpp

---
+++ Part-3/comment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // comment in cpp
+    int age = 25; // single line comment 
+
+    /* multi 
+    line
+     comment
+    . */
+       
+    cout << "Age: " << age << endl;
+    return 0;
+}
